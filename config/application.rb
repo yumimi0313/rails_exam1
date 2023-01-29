419e6531cb5182672a9c78215fec816bc98b214e
yumi yonemori <yumiyonemori@gmail.com>
2023-01-29 11:26:21 +0900

    タイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,7 @@ module RailsExam1
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
